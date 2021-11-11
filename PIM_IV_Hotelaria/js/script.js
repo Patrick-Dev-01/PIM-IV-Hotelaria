bc26db844293eeb865313ea5cbd2626cc88307a3
Patrick <patrick.rdecarvalho@outlook.com>
2021-11-11 19:36:56 -0300

    feature: pagina de reserva e ticket

---
+++ PIM_IV_Hotelaria/js/script.js
@@ -0,0 +1,28 @@
+﻿function adicionarOpcoes(quarto) {
+    const campoDescricao = document.getElementById("descricao");
+
+    if (quarto === "solteiro") {
+        campoDescricao.options = new Option('1 Cama de Solteiro', '1 cama de solteiro');
+    }
+
+    alert(quarto)
+}
+
+function Enviar() {
+    /*const mensagemErro = document.getElementById(`msg-erro-${campo}`);
+
+    const cpf = document.getElementById(`cpf`).value;
+    const nome = document.getElementById(`nome`).value;
+    const sobrenome = document.getElementById(`sobrenome`).value;
+    const nome = document.getElementById(`nome`).value;
+
+    if (valor.length < 10) {
+        mensagemErro.innerHTML = "CPF Incorreto";
+    }
+
+    else {
+        mensagemErro.innerHTML = "";
+    }*/
+
+    alert("clicou");
+}
